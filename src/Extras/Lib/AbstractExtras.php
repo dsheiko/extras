c9174d9da259d37a0c71ea3e772555297b18bc32
Dmitry Sheiko <dmitry@crytek.com>
2018-02-10 08:31:41 +0100

    morphologic chain

--- src/Extras/Lib/AbstractExtras.php
+++ src/Extras/Lib/AbstractExtras.php
@@ -2,6 +2,7 @@
 namespace Dsheiko\Extras\Lib;
 
 use Dsheiko\Extras\Lib\Chain;
+use Dsheiko\Extras\{Arrays, Collections, Strings, Functions};
 
 abstract class AbstractExtras
 {
@@ -13,7 +14,19 @@ abstract class AbstractExtras
      */
     public static function chain($value)
     {
-        $class = \get_called_class();
-        return new Chain($class, $value);
+        switch (true) {
+            case is_array($value):
+                return new Chain(Arrays::class, $value);
+            case is_string($value):
+                return new Chain(Strings::class, $value);
+            case $value instanceof \ArrayObject:
+            case $value instanceof \ArrayIterator:
+            case $value instanceof \Traversable:
+                return new Chain(Collections::class, $value);
+            case is_callable($value):
+                return new Chain(Functions::class, $value);
+        }
+
+        throw new \InvalidArgumentException("Cannot find passing collection for given type");
     }
 }
